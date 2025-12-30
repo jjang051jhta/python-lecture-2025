2466b08411e8b223834573d2e95177a6ad362c58
jjang051 <jjang051jhta@gmail.com>
2025-12-30 10:08:08 +0900

    seaborn

--- seaborn/seaborn02.py
+++ seaborn/seaborn02.py
@@ -6,14 +6,43 @@ import matplotlib.pyplot as plt
 plt.rcParams["font.family"] = "Malgun Gothic"
 plt.rcParams["axes.unicode_minus"] = False
 
+print(sns.get_dataset_names())  #seaborn에서 제공하는 데이터셋 목록 확인
 
-tips = sns.load_dataset("tips")  #seaborn에서 제공하는 tips 데이터셋 로드
-print(tips.head())
-figure = plt.figure(figsize=(15,5))
-ax01 = figure.add_subplot(1,2,1)  #가로 한줄에 2개의 그래프 배치 1번째 그래프
-ax02 = figure.add_subplot(1,2,2)  #가로 한줄에 2개의 그래프 배치 2번째 그래프
-sns.countplot(x="time", data=tips,ax=ax01)
-sns.countplot(x="time", data=tips,palette="Set2", hue="day", ax=ax02)
-ax01.set_title("시간별 팁의 빈도수")
-ax02.set_title("시간별 팁의 빈도수 (요일별)")
+tips = sns.load_dataset("tips")
+print(tips.head())  #tips 데이터셋의 처음 5행 출력
+
+fig = plt.figure(figsize=(15, 6))
+ax01 = fig.add_subplot(1,2,1)
+ax02 = fig.add_subplot(1,2,2)
+sns.countplot(data=tips, x="time", ax=ax01)
+sns.countplot(data=tips, x="time", ax=ax02, palette="Set2", hue="day")
+ax01.set_title("시간별 팁의 빈도 수")
+ax02.set_title("시간별 팁의 빈도 수 (요일별)")
+print("="*100)
+for p in ax01.patches:
+  height= p.get_height()
+
+  ax01.text(
+    p.get_x()+p.get_width()/2,
+    height + 0.1,
+    height,
+    ha="center",
+    va="bottom"
+  )
+for p in ax02.patches:
+  height= p.get_height()
+  if height==0:
+    continue
+    #p.set_visible(False)
+  ax02.text(
+    p.get_x()+p.get_width()/2,
+    height + 0.1,
+    height,
+    ha="center",
+    va="bottom"
+  )
+ax01.grid(axis="y", linestyle="--", alpha=0.5)
+ax02.grid(axis="y", linestyle="--", alpha=0.5)
+ax01.set_axisbelow(True)
+ax02.set_axisbelow(True)
 plt.show()
