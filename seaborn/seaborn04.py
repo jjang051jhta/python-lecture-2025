5dcc77c064bcbf239798eee56d64b27bd0e83ab9
jjang051 <jjang051jhta@gmail.com>
2025-12-30 11:19:15 +0900

    seaborn

---
+++ seaborn/seaborn04.py
@@ -0,0 +1,14 @@
+# pip install seaborn
+import seaborn as sns  #seaborn의 별칭은 sns 사람이름  
+import pandas as pd
+import matplotlib.pyplot as plt
+
+plt.rcParams["font.family"] = "Malgun Gothic"
+plt.rcParams["axes.unicode_minus"] = False
+
+#histogram: 연속형 변수를 구간(bins)으로 나누어 각 구간에 속하는 데이터의 빈도수를 막대그래프로 표현
+tips = sns.load_dataset("tips")
+sns.histplot(tips["tip"], bins=30, kde=False,stat="density", color="skyblue")  #kde: 커널 밀도 추정 그래프 추가
+sns.kdeplot(tips["tip"], color="red", lw=1)  #커널 밀도 추정 그래프
+plt.title("Histogram of Tips with KDE")
+plt.show()
